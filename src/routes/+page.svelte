f0f4523fe75e293841ad2b0e2089d5358bfa1a7e
Anthony Koch <anthonykoch@users.noreply.github.com>
2023-07-08 07:13:12 -0700

    Make error reproducible

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a href="ound the foll">kit.svelte.dev</a> to read the documentation</p>
+
+<a style="color: #9f9f9f" href="<%asm_global_unsubscribe_raw_url%>" target="_blank">
+  Unsubscribe
+</a>
